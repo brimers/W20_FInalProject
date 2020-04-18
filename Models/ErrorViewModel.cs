5501aa9ec9cc8d8a886111e90cad824cc124496d
Stephen <brimers@sheridancollege.ca>
2020-04-18 13:59:56 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinalProject_StephenBrimer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
